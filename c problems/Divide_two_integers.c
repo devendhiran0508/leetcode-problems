a698be6ad4a85fd9d30c790cea3c9a55d38b3130
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-03-27 19:14:34 +0530

    Delete c problems directory

--- c problems/Divide_two_integers.c
+++
@@ -1,9 +0,0 @@
-int divide(int dividend, int divisor) {
-    if (dividend == INT_MIN && divisor == -1)
-        return INT_MAX;
-
-    if (divisor == 0)
-        return 0;
-    else
-        return dividend / divisor;
-}
