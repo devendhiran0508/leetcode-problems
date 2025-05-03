9bf5938535db76773b0315396dda8597dd715aed
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-05-03 23:57:20 +0530

    Time: 40 ms (63.61%), Space: 53.7 MB (44.71%) - LeetHub

---
+++ 2775-generate-fibonacci-sequence/2775-generate-fibonacci-sequence.js
@@ -0,0 +1,20 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a=0
+    let b=1
+    while(true)
+    {
+        yield a;
+        const temp=a;
+        a=b
+        b+=temp;
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
