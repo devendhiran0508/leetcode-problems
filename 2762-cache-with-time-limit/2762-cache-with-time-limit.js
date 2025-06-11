ac3aa47288a6383cb92b11b2aa84e588a4ded235
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-06-11 19:36:01 +0530

    Time: 46 ms (59.47%), Space: 52.7 MB (90.11%) - LeetHub

---
+++ 2762-cache-with-time-limit/2762-cache-with-time-limit.js
@@ -0,0 +1,41 @@
+var TimeLimitedCache = function() {
+    this.cache=new Map();
+};
+
+/** 
+ * @param {number} key
+ * @param {number} value
+ * @param {number} duration time until expiration in ms
+ * @return {boolean} if un-expired key already existed
+ */
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+    let a=this.cache.has(key);
+    if(a)
+    {
+        clearInterval(this.cache.get(key).ref);
+    }
+    this.cache.set(key,{value,ref:setTimeout(()=>this.cache.delete(key),duration)});
+    return a;
+};
+
+/** 
+ * @param {number} key
+ * @return {number} value associated with key
+ */
+TimeLimitedCache.prototype.get = function(key) {
+    return this.cache.has(key)?this.cache.get(key).value:-1;
+};
+
+/** 
+ * @return {number} count of non-expired keys
+ */
+TimeLimitedCache.prototype.count = function() {
+    return this.cache.size;
+};
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
