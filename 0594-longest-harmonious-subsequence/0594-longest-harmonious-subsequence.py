acd8f33027e1f7deec02bd1e3b085bf5724fb9d3
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-06-30 20:42:49 +0530

    Time: 57 ms (18.68%), Space: 19.1 MB (72.21%) - LeetHub

---
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        nums.sort()
+        j=0
+        maxVal=0
+        for i in range(len(nums)):
+            while nums[i]-nums[j]>1:
+                j+=1
+            if nums[i]-nums[j]==1:
+                maxVal=max(maxVal,i-j+1)
+        return maxVal
