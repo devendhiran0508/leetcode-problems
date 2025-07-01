b301ff25ee9e5d20a09dcc71d5f8e4cd79aa47ba
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-07-01 13:37:09 +0530

    Time: 2855 ms (22.04%), Space: 17.8 MB (83.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -12,5 +12,5 @@ class Solution:
         for i in range(len(s),0,-1):
             for j in range(len(s)-i+1):
                 if check(j,i+j):
-                    return s[j:j+i]
+                    return s[j:i+j]
         return ""
