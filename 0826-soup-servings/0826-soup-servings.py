887b57ab8b3f751ccfddc37bfb0d727c633c49ef
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-08-08 19:11:12 +0530

    Time: 7 ms (93.07%), Space: 19.3 MB (76.9%) - LeetHub

---
+++ 0826-soup-servings/0826-soup-servings.py
@@ -0,0 +1,24 @@
+class Solution:
+    def soupServings(self, N: int) -> float:
+        if N >= 5000:  # Large enough, probability is almost 1
+            return 1.0
+        
+        from functools import lru_cache
+        N = (N + 24) // 25  # Scale down to units of 25 ml
+
+        @lru_cache(None)
+        def dp(a, b):
+            if a <= 0 and b <= 0:
+                return 0.5
+            if a <= 0:
+                return 1.0
+            if b <= 0:
+                return 0.0
+            return 0.25 * (
+                dp(a - 4, b) +
+                dp(a - 3, b - 1) +
+                dp(a - 2, b - 2) +
+                dp(a - 1, b - 3)
+            )
+        
+        return dp(N, N)
