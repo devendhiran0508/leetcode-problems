2ff4d6c4d5e21d0d5a7a5c18250049458b7d5608
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-08-27 10:30:15 +0530

    Time: 3931 ms (76.64%), Space: 571.4 MB (64.96%) - LeetHub

--- 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.py
+++ 3733-length-of-longest-v-shaped-diagonal-segment/3733-length-of-longest-v-shaped-diagonal-segment.py
@@ -1,29 +1,22 @@
 class Solution:
     def lenOfVDiagonal(self, grid: List[List[int]]) -> int:
-        m=len(grid)
-        n=len(grid[0])
-        dire=[(1,1),(1,-1),(-1,1),(-1,-1)]
-        nextVal={1:2,2:0,0:2}
+        dire = [(1,1),(1,-1),(-1,-1),(-1,1)]
+        m, n = len(grid), len(grid[0])
 
         @cache
-        def dfs(i,j,dirIdx,turned,val):
-            if not(0<=i<m and 0<=j<n) or grid[i][j]!=val:
+        def dfs(i, j, dirIdx, turned, val):
+            ni, nj = i + dire[dirIdx][0], j + dire[dirIdx][1]
+            if not (0 <= ni < m and 0 <= nj < n) or grid[ni][nj] != val:
                 return 0
-            res=1
-            nextI=i+dire[dirIdx][0]
-            nextJ=j+dire[dirIdx][1]
-            nxt=nextVal[val]
-            res=max(res,1+dfs(nextI,nextJ,dirIdx,turned,nxt))
+            res = dfs(ni, nj, dirIdx, turned, 2 - val)
+            if turned:
+                res = max(res, dfs(ni, nj, (dirIdx + 1) % 4, False, 2 - val))
+            return res + 1
 
-            if not turned:
-                nd=(dirIdx+1)%4
-                ti=i+dire[nd][0]
-                tj=j+dire[nd][1]
-                res=max(res,1+dfs(ti,tj,nd,True,nxt))
-            return res
-        ans=0
+        ans = 0
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==1:
-                    for k in range(4):ans=max(ans,dfs(i,j,k,False,1))
+                if grid[i][j] == 1:
+                    for k in range(4):
+                        ans = max(ans, 1 + dfs(i, j, k, True, 2))
         return ans
