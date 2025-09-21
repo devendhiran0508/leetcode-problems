567ecb32414c99081958fed500f59edbd55ab63d
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-09-21 14:28:21 +0530

    Time: 2520 ms (6.67%), Space: 114.2 MB (62.5%) - LeetHub

---
+++ 2023-design-movie-rental-system/2023-design-movie-rental-system.py
@@ -0,0 +1,67 @@
+from typing import List
+import heapq
+from collections import defaultdict
+
+class MovieRentingSystem:
+
+    def __init__(self, n: int, entries: List[List[int]]):
+        # priceMap: (shop, movie) -> price
+        self.price = {}
+        # available: movie -> min-heap of (price, shop)
+        self.available = defaultdict(list)
+        # rented: min-heap of (price, shop, movie)
+        self.rented = []
+        # track rented movies to handle removals from heap
+        self.rented_set = set()
+        
+        for shop, movie, price in entries:
+            self.price[(shop, movie)] = price
+            heapq.heappush(self.available[movie], (price, shop))
+    
+    def search(self, movie: int) -> List[int]:
+        res = []
+        temp = []
+        seen = set()
+        # pop top 5 cheapest available shops
+        while self.available[movie] and len(res) < 5:
+            price, shop = heapq.heappop(self.available[movie])
+            if (shop, movie) not in self.rented_set and (shop, movie) not in seen:
+                res.append(shop)
+                seen.add((shop, movie))
+            temp.append((price, shop))
+        # push back everything
+        for item in temp:
+            heapq.heappush(self.available[movie], item)
+        return res
+    
+    def rent(self, shop: int, movie: int) -> None:
+        self.rented_set.add((shop, movie))
+        heapq.heappush(self.rented, (self.price[(shop, movie)], shop, movie))
+    
+    def drop(self, shop: int, movie: int) -> None:
+        self.rented_set.remove((shop, movie))
+    
+    def report(self) -> List[List[int]]:
+        res = []
+        temp = []
+        seen = set()
+        while self.rented and len(res) < 5:
+            price, shop, movie = heapq.heappop(self.rented)
+            if (shop, movie) in self.rented_set and (shop, movie) not in seen:
+                res.append([shop, movie])
+                seen.add((shop, movie))
+            temp.append((price, shop, movie))
+        # push back everything
+        for item in temp:
+            heapq.heappush(self.rented, item)
+        return res
+
+        
+
+
+# Your MovieRentingSystem object will be instantiated and called as such:
+# obj = MovieRentingSystem(n, entries)
+# param_1 = obj.search(movie)
+# obj.rent(shop,movie)
+# obj.drop(shop,movie)
+# param_4 = obj.report()
