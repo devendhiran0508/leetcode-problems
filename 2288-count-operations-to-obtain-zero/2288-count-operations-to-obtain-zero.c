72fe93ec36ba46ef1a8cfe44eb84c02d03df50f5
Devendhiran Ganapathi <devendhiran0508@gmail.com>
2025-11-09 18:41:15 +0530

    Time: 0 ms (100%), Space: 7.9 MB (44.68%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.c
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.c
@@ -1,10 +1,10 @@
 int countOperations(int num1, int num2) {
-    int res = 0;  // total number of subtraction operations
+    int res = 0;  
     while (num1 && num2) {
-        // each step of the Euclidean algorithm
+        
         res += num1 / num2;
         num1 %= num2;
-        // swap two numbers
+        
         int temp = num1;
         num1 = num2;
         num2 = temp;
